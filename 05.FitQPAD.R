f151d8e7174759b0117bd434fa5c18ebfb9e4a2e
pthompson234 <47431897+pthompson234@users.noreply.github.com>
2026-07-29 12:18:30 -0700

    Update 05.FitQPAD.R

--- 05.FitQPAD.R
+++ 05.FitQPAD.R
@@ -22,13 +22,13 @@ T_SCALE = 1 / 60 # seconds to minutes
 DEG_CRS = "EPSG:4326"
 INITS = c(0, 1)
 MIN_TOD_OBS = 5 # minimum # of observations for a given time of day for the models to work; if there are fewer than 5 observations for a given time of day category, those will be removed
+DEG_DIGITS = 4 # how to round degrees, I know it's not an exact spatial unit but close enough; this is equal to about 100 m
 
 all_species = unique(pc.good.final$species_code)
-# all_species = c("CONW")
 if (!is.na(commandArgs()[6])) all_species = str_split_1(commandArgs()[6], " ")
 
-message("length(commandArgs()) = ", length(commandArgs()))
-print(commandArgs())
+# message("length(commandArgs()) = ", length(commandArgs()))
+# print(commandArgs())
 
 lambda_covs_formula = ~ 0 + morning + sunrise + day + sunset + night + nauticaldawn + nauticaldusk + is_pointcount + timeofyear
 alpha_covs_formula = ~ open_closed
@@ -94,15 +94,21 @@ all_final = rbind(pc_final, aru_final) %>%
   ungroup %>%
   arrange(-n_this_sp, species_code)
 
-rm(pc.good.final, wt.wide, aru.good, pc_final, aru_final)
+rm(pc.good.final, aru.good, pc_final, aru_final)
 gc()
 
 # Extract covariates ----
 timeofday_cov = cov_tod_bin("timeofday")
-all_final = all_final %>% mutate(timeofday = timeofday_cov$get(longitude, latitude, date, crs_in = DEG_CRS))
 
 closed_cov = cov_dynamic_raster(cov_name = "open_closed", raster_dir = file.path("data", "scanfi_biomass_agg"), method = "nearest")
-all_final$open_closed = with(all_final, closed_cov$get(longitude, latitude, date, crs_in = DEG_CRS))
+
+all_final = all_final %>%
+  mutate(timeofday = timeofday_cov$get(longitude, latitude, date, crs_in = DEG_CRS),
+         open_closed = closed_cov$get(longitude, latitude, date, crs_in = DEG_CRS))
+
+wt.wide = wt.wide %>%
+  mutate(timeofday = timeofday_cov$get(longitude, latitude, date, crs_in = DEG_CRS),
+         open_closed = closed_cov$get(longitude, latitude, date, crs_in = DEG_CRS))
 
 # Set up parallelization
 ncores = as.numeric(Sys.getenv("SLURM_CPUS_PER_TASK")) * as.numeric(Sys.getenv("SLURM_NTASKS_PER_NODE"))
@@ -115,35 +121,97 @@ qpad_fits = foreach(sp = all_species, .errorhandling = "stop") %dopar% {
   
   if (file.exists(qpad_fit_dir_out)) {
     message(sp, " is already done. ", Sys.time())
-  } else {
-    message("beginning ", sp, ": ", Sys.time())
-    
-    all_rtmb_ready = all_final %>%
-      dplyr::filter(species_code == sp) %>% 
-      group_by(timeofday) %>%
-      mutate(n_this_tod = n()) %>%
-      ungroup %>%
-      dplyr::filter(n_this_tod >= MIN_TOD_OBS) %>%
-      mutate(nauticaldawn = timeofday == "nauticaldawn",
-             sunrise = timeofday == "sunrise",
-             morning = timeofday == "morning",
-             day = timeofday == "day",
-             sunset = timeofday == "sunset",
-             nauticaldusk = timeofday == "nauticaldusk",
-             night = timeofday == "night",
-             open_closed = open_closed / max(open_closed),
-             timeofyear = yday(date) - 152,
-             is_pointcount = (type == "pc"))
-    
-    obj = try(fit_jqpadmix(all_rtmb_ready, formula_alpha = alpha_covs_formula, formula_lambda = lambda_covs_formula, return_data = TRUE, inits = INITS, profile_improve_stop = 1, return_hess = TRUE, return_ci = TRUE))
-    # obj_null = try(fit_jqpadmix(all_rtmb_ready, return_data = TRUE, inits = INITS, profile_improve_stop = 1, return_hess = TRUE, return_ci = TRUE))
-    
-    message("completed ", sp, ": ", Sys.time())
-    # saveRDS(list(full = obj, null = obj_null), qpad_fit_dir_out)
-    saveRDS(list(full = obj), qpad_fit_dir_out)
+    return(0)
   }
-  0
-  # qpad_fits = list(full = obj_pc, null = obj_pc_null)
+  
+  message("beginning ", sp, ": ", Sys.time())
+  
+  this_detections = all_final %>%
+    dplyr::filter(species_code == sp) %>%
+    group_by(survey_id) %>%
+    mutate(count_total = sum(count)) %>%
+    ungroup %>%
+    mutate(lon_rounded = round(longitude, DEG_DIGITS),
+           lat_rounded = round(latitude, DEG_DIGITS),
+           year = year(date)) %>%
+    group_by(lon_rounded, lat_rounded, year) %>%
+    mutate(max_count = max(count_total)) %>%
+    ungroup
+  
+  # Find the surveys at each location-year combo that returned the max count for the species, and then figure out how long those surveys lasted
+  rounded_duration_info = this_detections %>%
+    group_by(lon_rounded, lat_rounded, year) %>%
+    dplyr::filter(count_total == max_count) %>%
+    summarize(t_mxm = min(t_max),
+              best_survey_id = survey_id[which.min(t_max)])
+  
+  this_detections = this_detections %>%
+    left_join(rounded_duration_info, by = join_by(lon_rounded == lon_rounded, lat_rounded == lat_rounded, year == year))
+  
+  rounded_survey_info = this_detections %>%
+    group_by(lon_rounded, lat_rounded, year) %>%
+    summarize(n_surveys = n_distinct(survey_id),
+              max_count = max(max_count)) %>%
+    left_join(rounded_duration_info, by = join_by(lon_rounded == lon_rounded, lat_rounded == lat_rounded, year == year))
+  
+  # Add surveys with 0 birds observed for additional right censored samples
+  wt.wide$this_species = wt.wide[, sp, drop = TRUE] # so we have a consistent column name for upcoming dplyr operations
+  
+  rc_surveys = wt.wide %>%
+    mutate(lon_rounded = round(longitude, DEG_DIGITS),
+           lat_rounded = round(latitude, DEG_DIGITS),
+           year = year(date_time),
+           .after = distance) %>%
+    left_join(rounded_survey_info, by = join_by(lon_rounded == lon_rounded, lat_rounded == lat_rounded, year == year)) %>%
+    dplyr::filter(n_surveys > 0, this_species == 0)
+  
+  # align formatting with all_rtmb_ready
+  all_rtmb_rc = rc_surveys %>%
+    mutate(distance = ifelse(is.finite(distance), distance, MAX_DIST) / 1000,
+           duration = duration / 60,
+           obs_id = paste0("RC_", seq_len(n())),
+           r_lo = 0,
+           r_up = distance,
+           r_max = distance,
+           t_lo = duration,
+           t_up = Inf,
+           t_max = duration,
+           count = 0,
+           count_total = 0,
+           r_mxs = distance,
+           type = ifelse(method == "PC", "pc", "aru"),
+           equipment_model = NA) %>%
+    dplyr::select(obs_id, survey_id, best_survey_id, project_id, r_lo, r_up, r_max, t_lo, t_up, t_max, t_mxm, count, r_mxs, longitude, latitude, date = date_time, type, task_method = method, equipment_model, count_total, lon_rounded, lat_rounded, year, max_count)
+  
+  all_rtmb_ready = rbind(this_detections, all_rtmb_rc) %>%
+    group_by(timeofday) %>%
+    mutate(n_this_tod = sum(pmin(count, 1))) %>%
+    ungroup %>%
+    dplyr::filter(n_this_tod >= MIN_TOD_OBS) %>%
+    mutate(nauticaldawn = timeofday == "nauticaldawn",
+           sunrise = timeofday == "sunrise",
+           morning = timeofday == "morning",
+           day = timeofday == "day",
+           sunset = timeofday == "sunset",
+           nauticaldusk = timeofday == "nauticaldusk",
+           night = timeofday == "night",
+           open_closed = open_closed / max(open_closed),
+           timeofyear = yday(date) - 152,
+           is_pointcount = (type == "pc"))
+  
+  obj = try(fit_jqpadrc(all_rtmb_ready, 
+                        formula_alpha = alpha_covs_formula, 
+                        formula_lambda = lambda_covs_formula,
+                        return_data = TRUE, 
+                        inits = INITS, 
+                        profile_improve_stop = 1, 
+                        return_hess = TRUE, 
+                        return_ci = TRUE))
+
+  message("completed ", sp, ": ", Sys.time())
+  saveRDS(list(full = obj), qpad_fit_dir_out)
+  
+  obj
   
 }
 
